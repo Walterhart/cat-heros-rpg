ecf58aef97beb3bcf9fbb179a24e3b7f98ba19a9
Walterhart <walterwhart1@gmail.com>
2023-05-31 13:56:06 -0400

    Refactor getDiceRollArray() to look clearer

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 function getDiceRollArray(diceCount) {
-    let newDiceRolls = [];
-    for (let i = 0; i < diceCount; i++) {
-        newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
-    }
-    return newDiceRolls;
+  return new Array(diceCount).fill(0).map(function () {
+    return Math.floor(Math.random() * 6) + 1;
+  });
 }
-
-function getDiceHtml(diceCount){
-  return getDiceRollArray(diceCount).map(function(num){
-      return `<div class="dice">${num}</div>`
-  }).join('')
+function getDiceHtml(diceCount) {
+  return getDiceRollArray(diceCount)
+    .map(function (num) {
+      return `<div class="dice">${num}</div>`;
+    })
+    .join("");
 }
-
-
 
 const hero = {
   elementId: "hero",
   name: "Wizard",
   avatar: "./assets/images/playerwizard.jpg",
   health: 60,
-  diceCount: 3
+  diceCount: 3,
 };
 
 const monster = {
@@ -27,7 +24,7 @@ const monster = {
   name: "Evil Knight",
   avatar: "./assets/images/evilKnight.jpg",
   health: 10,
-  diceCount: 1
+  diceCount: 1,
 };
 
 function renderCharacter(data) {
