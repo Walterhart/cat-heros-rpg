ea009076f317f69618d94f0809b668d2d6cd7689
Walterhart <walterwhart1@gmail.com>
2023-05-31 17:18:11 -0400

    Move constructor  and getDiceRollArray() to own file

--- index.js
+++ index.js
@@ -1,38 +1,6 @@
 
 import characterData from './data/data.js'
-
-function getDiceRollArray(diceCount) {
-  return new Array(diceCount).fill(0).map(function () {
-    return Math.floor(Math.random() * 6) + 1;
-  });
-}
-function getDiceHtml(diceCount) {
-  return getDiceRollArray(diceCount)
-    .map(function (num) {
-      return `<div class="dice">${num}</div>`;
-    })
-    .join("");
-}
-
-
-
-function Character(data) {
-  Object.assign(this, data);
-  this.getCharacterHtml = function () {
-    const { elementId, name, avatar, health, diceCount } = this;
-    const diceHtml = getDiceHtml(diceCount);
-
-    return `
-    <div class="character-card">
-          <h4 class="name"> ${name} </h4>
-          <img class="avatar" src="${avatar}" />
-          <div class="health">health: <b> ${health} </b></div>
-          <div class="dice-container">    
-              ${diceHtml}
-          </div>
-      </div>`;
-  };
-}
+import Character  from './utils/Character.js'
 
 const wizard = new Character(characterData.hero);
 const evilKnight = new Character(characterData.monster);
