01720660004df6462f43a9e6d90f82ae76f86494
Walterhart <walterwhart1@gmail.com>
2023-06-01 12:22:40 -0400

    Add attack roll functionality

--- data/data.js
+++ data/data.js
@@ -4,6 +4,7 @@ const characterData = {
       avatar: "./assets/images/playerwizard.jpg",
       health: 60,
       diceCount: 3,
+      currentDiceScore: []
     },
   
     monster: {
@@ -11,6 +12,7 @@ const characterData = {
       avatar: "./assets/images/evilKnight.jpg",
       health: 10,
       diceCount: 1,
+      currentDiceScore: []
     },
   };
   export default characterData
--- index.js
+++ index.js
@@ -5,6 +5,14 @@ import Character  from './utils/Character.js'
 const wizard = new Character(characterData.hero);
 const evilKnight = new Character(characterData.monster);
 
+function attack() {
+  wizard.getDiceHtml()
+  evilKnight.getDiceHtml()
+  render()
+}
+
+document.getElementById('attack-button').addEventListener('click', attack)
+
 function render() {
   document.getElementById('hero').innerHTML =
     wizard.getCharacterHtml();
